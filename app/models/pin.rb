e826997c36c97cc1e84fc065deea5a3f15bfbbcd
Shoaib Shaikh <shoshaikh7@gmail.com>
2015-11-05 15:50:59 -0500

    Added so that user can add image to their pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,6 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
+  has_attached_file :image, styles: {medium: "300x300>"}
+  # Makes sure it is an actual image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
